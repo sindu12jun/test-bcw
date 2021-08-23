11abf8337802374201091edf4d139f120a4dbcc3
David <david.l@binance.com>
2021-08-23 18:33:01 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect} from "react";
+import {tx} from '@binance-chain/bcw';
+
+const myPrepareParams = {
+    "from": "0xFf21ddc5b114a5E4F1ee813180276dDdE7160C73",
+    "to": "0xFf21ddc5b114a5E4F1ee813180276dDdE7160C73",
+    "value": "0.01",
+    "network": {"chainType": "eth", "url": "https://eth-mainnet.raptoken.io"},
+    "asset": {
+        "networkSymbol": "ETH",
+        "contractAddress": "0x0000000000000000000000000000000000000000",
+        "decimals": 18,
+    }
+
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        const f = async () => {
+            const {send} = await tx.getGasFees(myPrepareParams)
+        }
+        f()
+    }, [])
+    return (
+        <div className="App">
+        </div>
+    );
 }
 
 export default App;
